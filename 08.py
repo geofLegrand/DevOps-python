8fdcd642795e08ecd5a5cbd65b2dc2cca6b48671
geoflegrand <gessongue@gmail.com>
2024-07-24 14:16:14 +0100

    update
    Signed-off-by: geoflegrand <gessongue@gmail.com>

--- 08.py
+++ 08.py
@@ -3,17 +3,26 @@
 kg_value = input("Give your kgs value ?  ")
 
 """
- we know  1kg = 2.20462 pound
+ as we know  1kg = 2.20462 pound
 
 """
+
+CONST = 2.20462
+
+# remove any space
+kg_value = kg_value.strip()
+
 # remove . , and - in different number before you test it if it's digit
 a1 = kg_value.replace(".","").replace(",","").replace("-","")
 
 if a1.isdigit():
+   
 
-   pouds = 2.20462 * float(kg_value)
+   pounds = float(kg_value) * CONST 
+
    # i add :.3f just to format the result to add 3 digit after the point in decimal number
    print(f"The convert is : {kg_value}kgs = {pouds:.3f} pounds")
+
 else:
     print("Please enter a numeric value !!!!")
 
